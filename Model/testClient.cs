b8968d973462a6e2a5dfc1379bd201f8a7042483
sam <15027@ecam.be>
2017-10-19 21:23:33 +0200

    first test

---
+++ Model/testClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.framework;
+
+namespace Model
+{
+    [TestFixture()]
+    public class TestClient
+    {
+        [testc()]
+        public void TestWriteName()
+        {
+            Assert.AreEqual("", Client.writeName(""));
+        }
+    }
+}
