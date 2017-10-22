04a9537d2a0ce25c1c1c62978205263d625312a2
Martin Degeldt <15076@ecam.be>
2017-10-22 10:41:17 +0200

    Now read directors

--- Entreprise/Entreprise/Program.cs
+++ Entreprise/Entreprise/Program.cs
@@ -11,11 +11,29 @@ namespace Entreprise
     {
         static void Main(string[] args)
         {
-
+            List<Directeur> di = new List<Directeur>();
+            DF difin;
+            DRH direhu;
             string[] lines = System.IO.File.ReadAllLines(@"../inpput.txt");
             foreach (string line in lines)
             {
+                string[] words = line.Split(' ');
 
+                if (words[0] == "DI")
+                {
+                    Directeur directeur = new Directeur(words[1], words[2]);
+                    di.Add(directeur);
+                }
+
+                if (words[0] == "DF")
+                {
+                    difin = new DF(words[1], words[2]);
+                }
+
+                if (words[0] == "DH")
+                {
+                    direhu = new DRH(words[1], words[2]);
+                }
             }
         }
     }
