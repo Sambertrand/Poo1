0918dbc2f0d48a41f2999c29590b83a677a600d3
Martin Degeldt <15076@ecam.be>
2017-10-22 17:32:22 +0200

    Better look report

--- Entreprise/Entreprise/Manager.cs
+++ Entreprise/Entreprise/Manager.cs
@@ -72,7 +72,8 @@ namespace Entreprise
                 {
                     if (mis.EndDate > now)
                     {
-                        line = sub.ToString() + " : " + mis.Client.Name + " du " + mis.StartDate.ToString() + " au " + mis.EndDate.ToString();
+                        line = sub.ToString() + " : " + mis.Client.Name + " du " + mis.StartDate.ToString("MM/dd/yyyy") +
+                            " au " + mis.EndDate.ToString("MM / dd / yyyy");
                         lines.Add(line);
                     }
                 }
